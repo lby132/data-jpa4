f84211e7f1a9f2e722b5ea59afde606e3a7a9fa2
lby132 <quddbscjswow@naver.com>
2023-06-07 00:42:30 +0900

    Initial commit

---
+++ src/main/java/study/datajpa4/DataJpa4Application.java
@@ -0,0 +1,13 @@
+package study.datajpa4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpa4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpa4Application.class, args);
+    }
+
+}
---
+++ src/main/java/study/datajpa4/entity/Team.java
@@ -0,0 +1,30 @@
+package study.datajpa4.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
